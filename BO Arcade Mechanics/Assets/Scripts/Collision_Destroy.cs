15e742b4c12f4c6ac47decbeff062e04c620e6b8
Mitchel Klijn <mitchelklijn2004@gmail.com>
2022-03-30 08:55:18 +0200

    Collider gefixed en obstakels gemaakt

--- BO Arcade Mechanics/Assets/Scripts/Collision_Destroy.cs
+++ BO Arcade Mechanics/Assets/Scripts/Collision_Destroy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Collision_Destroy : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
     }
--- BO Arcade Mechanics/Assets/Scripts/Collision_Detector.cs
+++ BO Arcade Mechanics/Assets/Scripts/Collision_Detector.cs
@@ -1,29 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Collision_Detector : MonoBehaviour
 {
     Rigidbody rb;
+    Image image;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        image = GameObject.Find("Empty Health Bar").GetComponent<Image>();
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "HP")
+        if (other.gameObject.tag == "Ob")
         {
-            Debug.Log("+25 Gezondheidspunten");
+            if (image.rectTransform.sizeDelta.x < 390)
+            {
+                float size = image.rectTransform.sizeDelta.x + 39;
+                image.rectTransform.sizeDelta = new Vector2(size, 40);
+            }
         }
 
-        if (collision.gameObject.tag == "PU")
+        if (other.gameObject.tag == "HP")
+        {
+            //Debug.Log("+25 Gezondheidspunten");
+
+            if (image.rectTransform.sizeDelta.x > 0)
+            {
+                float size = image.rectTransform.sizeDelta.x - 39;
+                image.rectTransform.sizeDelta = new Vector2(size, 40);
+            }
+        }
+
+        if (other.gameObject.tag == "PU")
         {
             //Debug.Log("Kracht Omhoog");
         }
 
-        if (collision.gameObject.tag == "HD")
+        if (other.gameObject.tag == "HD")
         {
             //Debug.Log("Hieperrijden");
             rb.velocity = new Vector3(0, 0, 10);
