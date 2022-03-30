c758c7adbded42b12507c7cd23162c93ff0ac4f9
Mitchel Klijn <mitchelklijn2004@gmail.com>
2022-03-30 11:53:07 +0200

    Lasers groen gemaakt

--- BO Arcade Mechanics/Assets/Scripts/Shooting/Lazers.cs
+++ BO Arcade Mechanics/Assets/Scripts/Shooting/Lazers.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class Lazers : MonoBehaviour
 {
     public float LifeTime;
-    // Start is called before the first frame update
+
     void Start()
     {
         transform.Rotate(90f, 0f, 0f);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
